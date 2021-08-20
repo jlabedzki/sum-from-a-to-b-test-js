dca4f3e198b3a728a3ddc578a9c36e4a9391c74f
Johnny <johnnylabedzki@hotmail.com>
2021-08-20 16:29:25 -0700

    Created a function to some the numbers between a given range recursively.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+
+
+
+
+const sum = (fromN, toN) => {
+  if (fromN < toN) {
+    return sum(fromN + 1, toN) + (fromN);
+  }
+
+  return toN;
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
